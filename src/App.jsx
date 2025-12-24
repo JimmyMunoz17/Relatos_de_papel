56ce0c51dce558c38aa61e091d6ffd2baa5a4bb0
Jimmy Muñoz <jmsoftwaredv@hotmail.com>
2025-12-23 21:03:27 -0500

    Relatos de papel configuración inicial con Tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex h-screen justify-center">
+        <h1 className="text-3xl font-bold">RELATOS DE PAPEL</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
