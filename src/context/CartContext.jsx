309a60c6a0680258affbfe0ab6d292cedfcc138e
GodoyGerman <german.godoyb93@gmail.com>
2025-12-30 21:07:52 -0500

    persistencia del carrito usando localStorage

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,9 +1,11 @@
-import { createContext, useContext, useReducer } from 'react';
+import { createContext, useContext, useReducer, useEffect } from 'react';
 
 const CartContext = createContext();
 
+// Estado inicial leyendo desde localStorage
+
 const initialState = {
-    items: []
+    items: JSON.parse(localStorage.getItem('cart')) || []
 };
 
 function cartReducer(state, action) {
@@ -31,14 +33,30 @@ function cartReducer(state, action) {
     }
 }
 
+//  Estado inicial leyendo desde localStorage
+
+
+
 export function CartProvider({ children }) {
     const [state, dispatch] = useReducer(cartReducer, initialState);
+
+    // Guardar carrito en localStorage cuando cambie
+    useEffect(() => {
+        localStorage.setItem(
+            'cart',
+            JSON.stringify(state.items)
+        );
+    }, [state.items]);
+
+    // API Frontend para el carrito
 
     const addItem = (item) => dispatch({ type: 'ADD', item });
     const removeItem = (id) => dispatch({ type: 'REMOVE', id });
     const increase = (id) => dispatch({ type: 'INCREASE', id });
     const decrease = (id) => dispatch({ type: 'DECREASE', id });
     const clear = () => dispatch({ type: 'CLEAR' });
+
+    // Valores derivados del estado
 
     const totalItems = state.items.reduce((s, i) => s + i.quantity, 0);
     const totalPrice = state.items.reduce((s, i) => s + i.quantity * i.price, 0);
